ba1626aa0093e8f04ba7fe59a1b0255447c41493
Omansh Krishn <omansh11597@gmail.com>
2023-12-08 07:50:00 +0530

    Update build script
    Signed-off-by: Omansh Krishn <omansh11597@gmail.com>

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 # Variables
 DEFCONFIG="nexus_defconfig"
-TOOLCHAIN="kdrag0n/proton-clang"
-AK3="veshrao/AK3"
+TOOLCHAIN="clangsantoni/zyc_clang"
+AK3="omansh-krishn/Anykernel3"
 TOOLCHAIN_DIR="/home/$USER/toolchain"
 AK3_DIR="/home/$USER/AnyKernel3"
 KDIR="$(pwd)"
@@ -26,22 +26,23 @@ if [ -d "$TOOLCHAIN_DIR" ]; then
     echo "[!] Toolchain directory exists. Skipping..."
 else
     echo "[…] Cloning ${TOOLCHAIN}..."
-    git clone https://github.com/"${TOOLCHAIN}" "${TOOLCHAIN_DIR}" --depth=1 >/dev/null 2>&1
+    git clone https://gitlab.com/"${TOOLCHAIN}" "${TOOLCHAIN_DIR}" -b 13 --depth=1 >/dev/null 2>&1
 fi
 
 if [ -d "$AK3_DIR" ]; then
     echo "[!] AnyKernel3 directory exists. Skipping..."
 else
     echo "[…] Cloning ${AK3}"
-    git clone https://github.com/"${AK3}" "${AK3_DIR}" --depth=1 >/dev/null 2>&1
+    git clone https://github.com/"${AK3}" "${AK3_DIR}" -b santoni-nexus --depth=1 >/dev/null 2>&1
 fi
 
 # Building
 PATH="${TOOLCHAIN_DIR}/bin/:$PATH"
 export KBUILD_COMPILER_STRING="${TOOLCHAIN_DIR}/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
-export KBUILD_BUILD_USER="vesh"
+export KBUILD_BUILD_USER="omansh-krishn"
 export KBUILD_BUILD_HOST="projects-nexus"
-read -rp "[?] Insert kernel version: " VERSION
+#read -rp "[?] Insert kernel version: " VERSION
+export VERSION=$(make kernelversion  | grep -oP '\d+\.\d+\.\d+')
 if [ -f out/arch/arm64/boot/Image.gz-dtb ]; then rm -rf out/arch/arm64/boot/Image.gz-dtb; fi
 
 echo "[…] Starting compilation..."
@@ -63,7 +64,7 @@ if [ -f out/arch/arm64/boot/Image.gz-dtb ]; then
     cd "${AK3_DIR}" || exit
     zip -rq9 "${KDIR}/../${ZIP}" * -x "README.md"  || { echo -e "[✘] Failed to create ZIP file."; exit 0; }
     cd "$KDIR" || exit
-    echo -n [i] "Link: $(curl -s -T "${KDIR}/../$ZIP" oshi.at | head -n -2 | tail -n -1 | cut -d' ' -f1)"
+    echo -n [i] "Link: $(curl --upload-file "${KDIR}/../$ZIP" https://free.keep.sh)"
         printf "\n"
     echo -n "[i] MD5: $(md5sum ../"${ZIP}" | cut -d' ' -f1)"
     echo "[✓] Kernel successfully zipped."
